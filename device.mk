def0151ec90853ac8c14be5346b231913d04ab4b
Glen Kuhne <kuh@google.com>
2017-04-07 14:54:17 -0700

    berlin: overlay: Set default hotspot SSID prefix
    Added a device specific overlay that changes the default name
    from "AndroidAp" to customized name
    
    Test: Builds, manually verified name change
    Bug: 36368950
    Change-Id: I38a5f6fb950d6a45342080bae186a8ed276d94bf

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: 2022-2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2022-2025 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -26,6 +26,7 @@ PRODUCT_PACKAGES += \
     LineageSystemUIBerlin \
     SettingsProviderResBerlin \
     SystemUIResBerlin
+    WifiResBerlin
 
 # Audio
 PRODUCT_COPY_FILES += \
