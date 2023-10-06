0f0d4abbb48151b678209a8ffcdc10d585cad041
Nolen Johnson <johnsonnolen@gmail.com>
2023-10-06 05:52:34 -0400

    berlin: T1RGS33M.135-109-9-1
    Change-Id: If67b0702a1ce10c3d51949fdbec19ca2b707f557

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,7 +56,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 90
 
 # Security
-VENDOR_SECURITY_PATCH := 2023-06-01
+VENDOR_SECURITY_PATCH := 2023-09-01
 
 # inherit from the proprietary version
 include vendor/motorola/berlin/BoardConfigVendor.mk
