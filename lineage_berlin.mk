575c9e0895559d8334c57941e986741ffc13f048
Nolen Johnson <johnsonnolen@gmail.com>
2023-12-28 21:47:46 -0500

    berlin: T1RGS33.135-109-9-7
    Change-Id: I863efaeb5c611230335086cfdafb63fca42b7dbd
    (cherry picked from commit 92fecc071299a470c78d93ffa2d8e467611df317)

--- lineage_berlin.mk
+++ lineage_berlin.mk
@@ -34,6 +34,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=berlin_retail \
-    PRIVATE_BUILD_DESC="berlin_global-user 13 T1RGS33.135-109-9-3 d791e1-b91f3 release-keys"
+    PRIVATE_BUILD_DESC="berlin_global-user 13 T1RGS33.135-109-9-7 9d4ba5-7c15b7 release-keys"
 
-BUILD_FINGERPRINT := motorola/berlin_global/berlin:13/T1RGS33.135-109-9-3/d791e1-b91f3:user/release-keys
+BUILD_FINGERPRINT := motorola/berlin_global/berlin:13/T1RGS33.135-109-9-7/9d4ba5-7c15b7:user/release-keys
