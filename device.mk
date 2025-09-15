7ba5e30cc7e8d172aef2542adc871350f6da403d
Nolen Johnson <johnsonnolen@gmail.com>
2025-09-15 00:51:58 -0400

    Revert "berlin: Disable ultrawide camera in Aperture"
    This reverts commit 5e0ddad5e993952bde141b279a05958a28a6ffcf.
    
    Reason: Fixed
    Change-Id: I8fd890f4b40dca5623de6a7248ef5b6c4b736841

--- device.mk
+++ device.mk
@@ -23,7 +23,6 @@ $(call inherit-product, device/motorola/sm7325-common/common.mk)
 # Overlay
 PRODUCT_PACKAGES += \
     FrameworksResDevice \
-    LineageApertureAppDevice \
     LineageSystemUIDevice \
     SystemUIResDevice \
     WifiResDevice
