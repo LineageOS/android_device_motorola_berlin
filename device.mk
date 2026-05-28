a379a28705592bcf184cc68acecf3ed62d4d70a2
Michael Bestas <mkbestas@lineageos.org>
2026-05-28 17:11:23 +0300

    berlin: Enable assistant key customization
    Change-Id: Ib4ef6f65369a40fc8c02a6617fb6e4f5b508e3e1

--- device.mk
+++ device.mk
@@ -23,6 +23,7 @@ $(call inherit-product, device/motorola/sm7325-common/common.mk)
 # Overlay
 PRODUCT_PACKAGES += \
     FrameworksResDevice \
+    LineagePlatformDevice \
     LineageSDKResDevice \
     SystemUIResDevice \
     WifiResDevice
